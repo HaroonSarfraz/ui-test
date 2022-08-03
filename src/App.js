399dd53bbb9bac2bd48612753e55058a1c4efc83
Haroon Sarfraz <haroonsarfraz10@gmail.com>
2022-08-03 21:57:18 +0500

    added background color

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { sampleData } from './constants/sampleData';
 
 export const App = () => {
   return (
-    <div className='px-2 md:px-[85px] py-20 md:py-[108px]'>
+    <div className='px-2 md:px-[85px] py-20 md:py-[108px] bg-[#E5E5E5]'>
       <Header />
 
-      <div className='my-[80px] grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3'>
+      <div className='my-[80px] grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 justify-items-center items-center'>
         {sampleData.map((data, index) => (
           <Card key={index} data={data} />
         ))}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <div className='flex justify-center items-center'>
-    <div className='max-w-[760px] px-4'>
+    <div className='max-w-[730px] mx-4'>
       <p className='font-bold font-poppins text-center md:leading-[55px] text-3xl md:text-[56px] text-[#1B264F]'>
         We make creative media that{' '}
         <span className='text-[#506BCA]'>adds value</span>
